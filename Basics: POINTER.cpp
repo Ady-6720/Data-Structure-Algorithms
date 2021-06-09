db11e818ebe5d51c669a8974038c05712e188a6f
Aditya Malode <82941123+Ady-6720@users.noreply.github.com>
2021-06-09 18:22:26 +0530

    Create Basics: POINTER.cpp

---
+++ Basics: POINTER.cpp
@@ -0,0 +1,39 @@
+//POINTERS
+//pointer is a address variable , it will store addres  of data not the data ,they are used to access data indirectly
+//pointers are used to access HEAP memory
+//HEAP MEMORY
+//Pointer is also used to access some data which is external to program or accessing the resources
+//Pointers are also used for passing parameters
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+
+int main() {
+   int a=10; //Variable 
+   int *p; //Declaration of Pointer 
+   p=&a; //initializing pointer to store address of data at variable a
+   cout<<"Value of a:  "<<a<<endl;
+   cout<<"Adress stored at pointer P:  "<<p<<endl; //it will change everytime as it is dynamic
+   cout<<"Pointer value(Value at adress stored in p): "<<*p<<endl;//*p is it will go to the adress and will get the value 
+   
+   cout<<"\n\n-------Creating An array in the heap-----------\n\t\t\t\t\t&\n-------accessing Elements using for loop-------"<<endl;
+   int *q;
+   q=new int[5]; //we used this to access array inside heap
+  
+  
+  
+  /* OUTPUT:
+              Value of a:  10
+              Adress stored at pointer P:  0x7ffe334b9fe0
+              Pointer value(Value at adress stored in p): 10
+
+
+              -------Creating An array in the heap-----------
+                        &
+              -------accessing Elements using for loop-------
+              10
+              12
+              22
+              45
+              123
