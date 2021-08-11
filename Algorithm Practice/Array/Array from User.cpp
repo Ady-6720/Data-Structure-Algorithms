b7805a06ecf44fdf2f9844b718057052352958c2
Aditya Malode <82941123+Ady-6720@users.noreply.github.com>
2021-08-11 17:39:49 +0530

    Taking inputs for the array from the user and then printing the array elements on one single line
    Taking inputs for the array from the user and then printing the array elements on one single line

--- Algorithm Practice/Array/Array from User.cpp
+++ Algorithm Practice/Array/Array from User.cpp
@@ -1,1 +1,17 @@
+//Taking inputs for the array from the user and then printing the array elements on one single line
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n,i,j;
+    int marks[n];
+    cout<<"Enter number of marks you want to enter."<<endl;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>marks[i];
+    }
+    for(j=0;j<n;j++){
+        cout<<" ";
+        cout<<marks[j];
+    }
+}
