d83136ba3bb334a87786fae7b6a41bafca61caa8
Aditya Malode <82941123+Ady-6720@users.noreply.github.com>
2021-08-12 16:35:00 +0530

    Birthday Cake
    You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. 
      They will only be able to blow out the tallest of the candles. Count how many candles are tallest.

---
+++ Algorithm Practice/Array/Hackerrank-Birthday Cake.cpp
@@ -0,0 +1,27 @@
+/*
+  You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. 
+  They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long int i,big,tallc=0;
+    int n,a[n];
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    big=a[0];
+    for(i=0;i<n;i++){
+        if(big<a[i]|| big==a[i]){
+            big=a[i];
+        }
+    }
+    for(i=0;i<n;i++){
+        if(big==a[i])tallc++;
+    }    
+    cout<<tallc<<endl;
+    return 0;
+}
